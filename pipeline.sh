915cb9a3fe9d2896d6401b856f27ab45c69b1ac1
niladmiran <maosong@pjlab.org.cn>
2023-09-06 10:52:27 +0800

    refactor(pipeline.sh): complete pipeline
    1. add debug info
    2. add generate_bb.py modules

--- pipeline.sh
+++ pipeline.sh
@@ -13,7 +13,7 @@ debug_flag=$3
 # Get the directory of the input_tex file
 input_directory=$(dirname "$input_tex")
 
-# Get the filename of the input_tex file
+# Get the filename of the input_tex file without extension
 filename_with_extension=$(basename -- "$input_tex")
 input_filename="${filename_with_extension%.*}"
 
@@ -24,20 +24,22 @@ if [ "$?" -ne 0 ]; then
     exit 1
 fi
 
-# name of output tex file
-output_tex="$input_directory/${input_filename}_rendered.tex"
+# name of output tex file wthout extension
+output_filename="${input_filename}_rendered"
 
 # Run the Python script to render the .tex file
 if [ -z "$debug_flag" ]; then
-    python run_rendering.py --input_tex_file "$input_tex" --output_tex_file "$output_tex"
+    python run_rendering.py --input_tex_file "$input_tex" --output_tex_file "$input_directory/$output_filename"
 else
-    python run_rendering.py --input_tex_file "$input_tex" --output_tex_file "$output_tex" "$debug_flag"
+    python run_rendering.py --input_tex_file "$input_tex" --output_tex_file "$input_directory/$output_filename" "$debug_flag"
 fi
 
 # Check if the Python script execution was successful
 if [ "$?" -ne 0 ]; then
     echo "Error: Failed to render the .tex file."
     exit 1
+else
+    echo "Successfully rendered the .tex file."
 fi
 
 ############# compile the original .tex file into a PDF###########################
@@ -48,50 +50,62 @@ bash compile_latex.sh "$input_directory" "$input_filename"
 if [ "$?" -ne 0 ]; then
     echo "Error: Failed to compile the original .tex file into a PDF."
     exit 1
+else
+    echo "Successfully compiled the original .tex file into a PDF."
 fi
 
 # copy the original PDF file to the output directory
 mkdir -p "output/original"
-original_pdf="output/original/$input_filename.pdf"
+original_pdf="$input_directory/output/original/$input_filename.pdf"
 cp "$input_directory/$input_filename.pdf" "$original_pdf"
 
 ############# compile the rendered .tex file into a PDF###########################
 # Run the shell script to compile the output .tex file into a PDF
-bash compile_latex.sh "$input_directory" "$output_tex"
+bash compile_latex.sh "$input_directory" "$output_filename"
 
 # Check if the shell script execution was successful
 if [ "$?" -ne 0 ]; then
     echo "Failed to compile the output .tex file into a PDF."
     exit 1
+else
+    echo "Successfully compiled the output .tex file into a PDF."
 fi
 
 mkdir -p "output/rendered/"
-rendered_pdf="$input_directory/output/rendered/$output_tex.pdf"
-mv "$input_directory/$output_tex.pdf" "$rendered_pdf"
+rendered_pdf="$input_directory/output/rendered/$output_filename.pdf"
+mv "$input_directory/$output_filename.pdf" "$rendered_pdf"
 
-############# converting the original PDF and output PDF into images ################
-python pdf2image.py --pdf "$original_pdf" --output_path "$input_directory/output/original"
+############# convert the original PDF into images ################
+python pdf2jpg.py --pdf "$original_pdf" --output_path "$input_directory/output/original"
 
 # Check if the shell script execution was successful
 if [ "$?" -ne 0 ]; then
     echo "Error: Failed to converting the input .pdf file into images."
     exit 1
+else
+    echo "Successfully converted the input .pdf file into images."
 fi
 
-############# converting the rendered PDF and output PDF into images ################
-python pdf2image.py --pdf "$rendered_pdf" --output_path "$input_directory/output/rendered"
+############# convert the rendered PDF into images ################
+python pdf2jpg.py --pdf "$rendered_pdf" --output_path "$input_directory/output/rendered"
 
 # Check if the shell script execution was successful
 if [ "$?" -ne 0 ]; then
     echo "Error: Failed to converting the output .pdf file into images."
     exit 1
+else
+    echo "Successfully converted the output .pdf file into images."
 fi
 
 ############## generate the bounding box with original and rendered PDF ##########
-# python generate_bounding_box.py "$input_directory" "$output_directory"
+python generate_bb.py --path "$input_directory/output" --file_name "$input_filename"
 
-# # Check if the shell script execution was successful
-# if [ "$?" -ne 0 ]; then
-#     echo "Error: Failed to generate the bounding box."
-#     exit 1
-# fi
+# Check if the shell script execution was successful
+if [ "$?" -ne 0 ]; then
+    echo "Error: Failed to generate the bounding box."
+    exit 1
+else
+    echo "Successfully generated the bounding box."
+fi
+
+echo "Script completed successfully."
