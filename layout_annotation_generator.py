1c5f9b9c64ceec9e1cc8045a90fd6b06f4b17507
niladmiran <maosong@pjlab.org.cn>
2023-10-09 14:14:01 +0800

    feat(layout_annotation_generator.py): complex env bounding box generation

--- layout_annotation_generator.py
+++ layout_annotation_generator.py
@@ -1,58 +1,122 @@
-from PIL import Image, ImageDraw, ImageFont
+import os
+import glob
+import matplotlib.pyplot as plt
+import numpy as np
+from skimage.measure import label, regionprops
+import cv2
+import re
 from typing import Dict, List, Tuple
-
-from pdfminer.layout import LAParams, LTPage, LTComponent, LTFigure, LTLine
+import matplotlib.pyplot as plt
+from PIL import Image, ImageDraw
 
 from rendering.utils import load_json
 
 
-class AnnotationGenerator:
-    def __init__(self, config: dict) -> None:
-        self.config = config
-        self.text_info = load_json(config["text_elements_file"])
-        self.section_generator = self._list_generator(self.text_info["section"])
-        self.text_annotation = []
+config = load_json("config.json")
 
-    def _list_generator(self, list):
-        for item in list:
-            yield item
+name2category = {v: k for k, v in config["category_name"]}
+category2name = {k: v for k, v in config["category_name"]}
 
-    def generate_annotation(
-        self,
-        page_image: Image.Image,
-        page_elements: List[LTComponent],
-        category_info: Dict[int, int],
-    ) -> Image.Image:
-        for index, element in enumerate(page_elements):
-            category = category_info[index]
-            name = category2name[category]
-            self.generate_bb_annotation(page_image, element, name)
-            self.generate_text_annotation(element, name)
-        return page_image
 
-    def generate_bb_annotation(self, page_image, element, name):
-        draw = ImageDraw.Draw(page_image)
-        # use `locate .ttf` to find the available fonts
-        font = ImageFont.truetype(
-            self.config["annotation_image_font_type"],
-            self.config["annotation_image_font_size"],
-        )
+def get_image_pairs(dir1: str, dir2: str):
+    """
+    Generate a list of image pairs based on the directories provided.
 
-        draw.rectangle(element.bbox, outline="red")
-        draw.text(
-            (element.bbox[0], element.bbox[1]),
-            name,
-            fill=(255, 0, 0),
-            font=font,
-        )
+    Parameters:
+        dir1 (str): The directory path where the first set of images is located.
+        dir2 (str): The directory path where the second set of images is located.
 
-    def generate_text_annotation(self, element, name):
-        if name == "Others":
-            return
+    Raises:
+        FileNotFoundError: If the number of images in each directory does not
+            match or if the page index in the file names does not match.
 
-        if name == "Section":
-            self.generate_section_annotation(element)
+    Returns:
+        list: A list of tuples representing the image pairs.
+            Each tuple contains the page index, the path to the rendered image,
+            and the path to the changed image.
+    """
+    file_pattern = os.path.join(dir1, "*.png")
+    rendered_png_files = sorted(glob.glob(file_pattern))
+    file_pattern = os.path.join(dir2, "*.png")
+    changed_png_files = sorted(glob.glob(file_pattern))
 
-    def generate_section_annotation(self, element):
-        section_name = next(self.section_generator)
-        self.text_annotation.append({})
+    if len(rendered_png_files) != len(changed_png_files):
+        raise FileNotFoundError("Wrong image path or file name or page index!")
+
+    page_indices = []
+    for i in range(len(rendered_png_files)):
+        match = re.search(r"_(\d+)\.png$", rendered_png_files[i])
+        page_index = match.group(1)
+        page_indices.append(page_index)
+
+    for i in range(len(changed_png_files)):
+        match = re.search(r"_(\d+)\.png$", rendered_png_files[i])
+        page_index = match.group(1)
+        if page_index != page_indices[i]:
+            raise FileNotFoundError("Wrong image path or file name or page index!")
+
+    image_pairs = list(zip(page_indices, rendered_png_files, changed_png_files))
+    return image_pairs
+
+
+def generate_bounding_box(image_pairs, threshold=0.3):
+    result = {}
+    for image_pair in image_pairs:
+        page_index = image_pair[0]
+        result[page_index] = []
+
+        image1 = plt.imread(image_pair[1])
+        image1_array = np.array(image1, dtype=np.uint8)
+
+        image2 = plt.imread(image_pair[2])
+        image2_array = np.array(image2, dtype=np.uint8)
+
+        diff_image = np.abs(image2_array - image1_array, dtype=np.uint8)
+        binary_image = diff_image > threshold
+        labeled_image = label(binary_image)
+
+        regions = regionprops(labeled_image)
+        bounding_boxes = [region.bbox for region in regions]
+
+        if len(bounding_boxes) == 0:
+            continue
+
+        min_x = min(bounding_boxes, key=lambda x: x[1])[1]
+        min_y = min(bounding_boxes, key=lambda x: x[0])[0]
+        max_x = max(bounding_boxes, key=lambda x: x[4])[4]
+        max_y = max(bounding_boxes, key=lambda x: x[3])[3]
+
+        result[page_index].append((min_x, min_y, max_x, max_y))
+
+    return result
+
+
+def show_annotation(image_pair, bounding_boxes):
+    image_path = image_pair[2]
+    original_image = Image.open(image_path)
+
+    # Create a drawing object
+    draw = ImageDraw.Draw(original_image)
+
+    # Define the rectangle outline color (in RGB format)
+    outline_color = (255, 0, 0)  # Red
+
+    for bounding_box in bounding_boxes:
+        x1, y1, x2, y2 = bounding_box
+
+        # Draw the rectangle on the image
+        draw.rectangle([(x1, y1), (x2, y2)], outline=outline_color)
+
+    original_image.save("output.png")
+
+
+def run(dir1: str, dir2: str):
+    image_pairs = get_image_pairs(dir1, dir2)
+    annotation = generate_bounding_box(image_pairs)
+    show_annotation(image_pairs[4], annotation['4'])
+
+
+if __name__ == "__main__":
+    dir1 = os.path.expanduser("~/icml2022/output/rendered")
+    dir2 = os.path.expanduser("~/icml2022/output/changed")
+    run(dir1, dir2)
