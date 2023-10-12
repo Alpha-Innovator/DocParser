c3e60ee6ed082c14ab25108f0666c4efb290c621
niladmiran <maosong@pjlab.org.cn>
2023-10-12 17:09:19 +0800

    refactor(convert_pdf_to_image.sh): rename variables

--- convert_pdf_to_image.sh
+++ convert_pdf_to_image.sh
@@ -2,19 +2,19 @@
 
 # Check if the correct number of arguments is provided
 if [ "$#" -lt 1 ] || [ "$#" -gt 2 ]; then
-    echo "Usage: $0 <original_pdf> <output_directory>"
+    echo "Usage: $0 <input_pdf> <output_directory>"
     exit 1
 fi
 
-original_pdf=$1
+input_pdf=$1
 output_directory=$2
 
-python pdf2png.py --pdf "$original_pdf" --output_path "$output_directory"
+python pdf2png.py --pdf "$input_pdf" --output_path "$output_directory"
 
 # Check if the shell script execution was successful
 if [ "$?" -ne 0 ]; then
-    echo "[$0] Error: Failed to converting the $original_pdf file into images."
+    echo "[$0] Error: Failed to converting the $input_pdf file into images."
     exit 1
 else
-    echo "[$0] Successfully converted the $original_pdf file into images."
+    echo "[$0] Successfully converted the $input_pdf file into images."
 fi
