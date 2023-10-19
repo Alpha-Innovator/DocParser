4ec155a2bb381fa0cb29dcdd4136c1c0b437f1f6
niladmiran <maosong@pjlab.org.cn>
2023-10-19 10:31:48 +0800

    feat(batch_process.py): use regex to resolve input

--- scripts/batch_process.py
+++ scripts/batch_process.py
@@ -146,13 +146,72 @@ def main():
     extract_result(main_directory, destination_directory)
 
 
+def resolve_latex_imports(latex_file):
+    print(f"latex_file={latex_file}")
+    path = os.path.dirname(latex_file)
+    file_content = ""
+    with open(latex_file) as f:
+        file_content = f.read()
+
+    # Recursively resolve until no more imports found
+    while True:
+        prev_content = file_content
+        file_content = resolve_one_pass(path, file_content)
+
+        if file_content == prev_content:
+            break
+
+    with open(latex_file, "w") as f:
+        f.write(file_content)
+
+
+def resolve_one_pass(path, file_content):
+    # Find and resolve imports
+    input_pattern = r"\\input\{(.*?)\}"
+    for match_str in re.finditer(input_pattern, file_content):
+        input_file = match_str.group(1)
+        if not input_file.endswith(".tex"):
+            input_file = input_file + ".tex"
+        print(f"input_file={input_file}")
+        with open(os.path.join(path, input_file)) as f:
+            file_content = file_content.replace(match_str.group(), f.read())
+
+    # Find and resolve imports
+    import_pattern = r"\\import\{(.*?)\}"
+    for match_str in re.finditer(import_pattern, file_content):
+        import_file = match_str.group(1)
+        if not import_file.endswith(".tex"):
+            import_file = import_file + ".tex"
+        print(f"import_file={import_file}")
+        with open(os.path.join(path, import_file)) as f:
+            file_content = file_content.replace(match_str.group(), f.read())
+
+    # Find and resolve includes
+    include_pattern = r"\\include\{(.*?)\}"
+    for match_str in re.finditer(include_pattern, file_content):
+        include_file = match_str.group(1)
+        if not include_file.endswith(".tex"):
+            include_file = include_file + ".tex"
+        print(f"include_file={include_file}")
+        with open(os.path.join(path, include_file)) as f:
+            file_content = file_content.replace(match_str.group(), f.read())
+
+    # Find and resolve subimports
+    subimport_pattern = r"\\subimport\{(.*?)\}"
+    for match_str in re.finditer(subimport_pattern, file_content):
+        subimport_file = match_str.group(1)
+        if not subimport_file.endswith(".tex"):
+            subimport_file = subimport_file + ".tex"
+        print(f"subimport_file={subimport_file}")
+        with open(os.path.join(path, subimport_file)) as f:
+            file_content = file_content.replace(match_str.group(), f.read())
+
+    return file_content
+
+
 def resolve_tex_files(tex_files):
     for tex_file in tex_files:
-        log.debug(f"resolve file: {tex_file}")
-        try:
-            resolve(tex_file)
-        except:
-            log.debug(f"failed to resolve file: {tex_file}")
+        resolve_latex_imports(tex_file)
 
 
 if __name__ == "__main__":
