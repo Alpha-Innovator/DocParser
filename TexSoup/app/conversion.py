d6cba280574b154bf92ae659804a20d4c5dc87a6
niladmiran <maosong@pjlab.org.cn>
2023-10-31 18:07:25 +0800

    feat(conversion.py): simplify parsing process

--- TexSoup/app/conversion.py
+++ TexSoup/app/conversion.py
@@ -8,6 +8,7 @@ from TexSoup.TexSoup.data import TexGroup
 
 
 from logger import logger
+from rendering import envs
 
 log = logger.get_logger(__name__)
 
@@ -22,15 +23,27 @@ def remove_outer_curly_brackets(text):
 
 
 def to_list(tex_tree):
+    # TODO: simplify code logic, especially for text envs
     str_tree = []
     for i in tex_tree:
         if isinstance(i, list):
             str_tree.append(i)
         elif isinstance(i, TexEnv):
-            if i.args:
-                for index, item in enumerate(i.all):
-                    if isinstance(item, str) and item[0] == "\n":
-                        break
+            if i.name in envs.inline_math_envs:  # inline math mode
+                if isinstance(str_tree[-1], str):
+                    str_tree[-1] += str(i)
+                else:
+                    str_tree.append(str(i))
+            elif (
+                i.name
+                in envs.math_envs
+                + envs.algorithm_envs
+                + envs.tabular_envs
+                + envs.list_envs
+            ):
+                str_tree.append({i.name: str(i)})
+            else:
+                index = len(i.args)
                 str_tree.append(
                     {
                         i.name: [
@@ -40,41 +53,35 @@ def to_list(tex_tree):
                         ]
                     }
                 )
-            else:
-                str_tree.append(
-                    {
-                        i.name: [
-                            {"begin": i.begin},
-                            to_list(i.all),
-                            {"end": i.end},
-                        ]
-                    }
-                )
         elif isinstance(i, TexCmd):
             if i.name == "item":
-                str_tree.append(
-                    {
-                        i.name: [
-                            "\\" + i.name,
-                            to_list(i.contents),
-                        ]
-                    }
-                )
-                continue
-            if i.name == "def":
+                str_tree.append(str(i))
+            elif i.name == "def":
                 macro_name = remove_outer_curly_brackets(str(i.args[0]))
                 parameter_text = i.args[1].string
-                str_tree.append(
-                    {i.name: "\\" + i.name + macro_name + parameter_text}
-                )
-                continue
-            str_tree.append({i.name: "\\" + i.name + str(i.args)})
+                str_tree.append({i.name: "\\" + i.name + macro_name + parameter_text})
+            elif i.name in envs.ignore_envs:
+                if isinstance(str_tree[-1], str):
+                    str_tree[-1] += str(i)
+                else:
+                    str_tree.append(str(i))
+            else:
+                str_tree.append({i.name: "\\" + i.name + str(i.args)})
         elif isinstance(i, TexText):
-            str_tree.append(str(i.text))
+            if str_tree and isinstance(str_tree[-1], str):
+                str_tree[-1] += str(i.text)
+            else:
+                str_tree.append(str(i.text))
         elif isinstance(i, TexGroup):
             str_tree.append(["{", to_list(TexSoup(i.value).expr.all), "}"])
         else:
-            str_tree.append(str(i))
+            if str_tree and isinstance(str_tree[-1], str):
+                str_tree[-1] += str(i)
+            else:
+                str_tree.append(str(i))
+
+    if str_tree and isinstance(str_tree[-1], str):
+        pass
 
     return str_tree
 
