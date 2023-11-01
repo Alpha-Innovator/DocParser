ea5318104f4278732352da8b04f4e3ac33d7bfa3
niladmiran <maosong@pjlab.org.cn>
2023-11-01 15:46:57 +0800

    feat(run_preprocess.py): convert pdf image to png image

--- scripts/run_preprocess.py
+++ scripts/run_preprocess.py
@@ -1,33 +1,60 @@
 import argparse
+import subprocess
+import os
+import re
 
-from preprocess.crop_pdf_image import crop_pdf_image
-from preprocess.reduce_empty_lines import reduce_empty_lines
-from preprocess.remove_comment_line import remove_comment_line
-from preprocess.resolve_inputs import resolve_latex_imports
-from preprocess.comment_lines import comment_sections
+from pdf2image.pdf2image import convert_from_path
 
 
-def run(tex_file):
-    resolve_latex_imports(tex_file)
-    comment_sections(tex_file)
-    remove_comment_line(tex_file)
-    reduce_empty_lines(tex_file)
-    crop_pdf_image(tex_file)
+def replace_pdf_with_png(tex_file):
+    path = os.path.dirname(tex_file)
+    with open(tex_file) as f:
+        content = f.read()
+
+    # Regular expression pattern to match \includegraphics
+    # commands with PDF files
+    pattern = r"\\includegraphics(\[.*?\])?\{(.*?\.pdf)\}"
+
+    # Find all matches of \includegraphics with PDF files
+    matches = re.findall(pattern, content)
+
+    # Replace PDF paths with PNG paths
+    for match in matches:
+        # crop the pdf image
+        pdf_image_name = match[1]
+        pdf_image = os.path.join(path, pdf_image_name)
+        subprocess.run(["pdfcrop", pdf_image, pdf_image])
+
+        # convert the pdf image into png
+        png_image_name = os.path.splitext(pdf_image_name)[0] + ".png"
+        png_image = os.path.join(path, png_image_name)
+        convert_pdf_to_png(pdf_image, png_image)
+
+        # replace the reference in tex file
+        content = content.replace(match[1], png_image_name)
+
+    with open(tex_file, "w") as f:
+        f.write(content)
+
+
+def convert_pdf_to_png(pdf_image: str, png_image: str):
+    images = convert_from_path(pdf_image)
+    images[0].save(png_image)
 
 
 def parse_arguments():
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "--tex_file", type=str, required=True, help="Path to the input tex file"
+        "--input_tex", type=str, required=True, help="Path to the input tex file"
     )
     args = parser.parse_args()
-    tex_file = args.tex_file
+    tex_file = args.input_tex
     return tex_file
 
 
 def main():
     tex_file = parse_arguments()
-    run(tex_file)
+    replace_pdf_with_png(tex_file)
 
 
 if __name__ == "__main__":
