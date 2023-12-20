7727633656603bbd563557aee30c5ef7fe1fdf7e
MaoSong2022 <maosong@pjlab.org.cn>
2023-12-20 18:11:02 +0800

    feat(run_statistics.py): add statistics

---
+++ scripts/run_statistics.py
@@ -0,0 +1,69 @@
+from collections import defaultdict
+import os
+import re
+import matplotlib.pyplot as plt
+from rich import print
+import csv
+
+
+def analyze_result():
+    success_counts = defaultdict(int)
+    failure_counts = defaultdict(int)
+
+    prefix = r"/cpfs01/shared/ADLab/datasets/vrdu_arxiv/"
+
+    with open("success_files.txt", "r") as f:
+        for line in f:
+            category = re.match(
+                r"/cpfs01/shared/ADLab/datasets/vrdu_arxiv/(.*?)/", line
+            ).group(1)
+            success_counts[category] += 1
+
+    with open("failed_files.txt", "r") as f:
+        for line in f:
+            category = re.match(
+                r"/cpfs01/shared/ADLab/datasets/vrdu_arxiv/(.*?)/", line
+            ).group(1)
+            failure_counts[category] += 1
+
+    total_counts = defaultdict(int)
+
+    for category, count in success_counts.items():
+        total_counts[category] = success_counts[category] + failure_counts[category]
+
+    for category, count in failure_counts.items():
+        total_counts[category] = success_counts[category] + failure_counts[category]
+
+    data = {
+        category: (success_counts[category] / total_counts[category]) * 100
+        for category in total_counts
+    }
+
+    return data
+
+
+def analyze_raw_data(path):
+    categories = []
+    with open("scripts/category_count.csv", "r") as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            categories.append(row["categories"])
+
+    data = defaultdict(int)
+    for category in categories:
+        if os.path.exists(os.path.join(path, category)):
+            data[category] = len(os.listdir(os.path.join(path, category)))
+
+    return data
+
+
+data = analyze_raw_data(
+    "/cpfs01/shared/ADLab/datasets/arxiv_source/arxiv_source_uncompressed"
+)
+
+print(f"sum of data: {sum(data.values())}")
+
+print({k: v for k, v in data.items() if k.startswith("cs")})
+print(
+    f"sum of cs: {sum({k: v for k, v in data.items() if k.startswith('cs')}.values())}"
+)
