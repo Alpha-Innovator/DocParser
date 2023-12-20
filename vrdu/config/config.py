888476bb88639d94fd94686781f1917aa3ed765b
niladmiran <maosong@pjlab.org.cn>
2023-12-20 14:05:55 +0800

    Revert "Merge branch 'dev' of github.com:MaoSong2022/vrdu_data_process into dev"
    This reverts commit 4d8d8efaa56de49b1cce10c9b744d40b5f8f6fb4, reversing
    changes made to fc6daeff7404ce1372912d06f01fe1aaa1097046.

--- vrdu/config/config.py
+++ vrdu/config/config.py
@@ -60,26 +60,16 @@ relation_map = {
     ("Title", "Equation"): "sub",
     ("Title", "List"): "sub",
     ("Title", "Footnote"): "ref",
-    # Title category relationships
-    ("chapter", "chapter"): "peer",
-    ("section", "chapter"): "sub",
-    ("section", "section"): "peer",
-    ("subsection", "section"): "sub",
-    ("subsection", "subsection"): "peer",
-    ("subsubsection", "subsection"): "sub",
-    ("subsubsection", "subsubsection"): "peer",
+    ("chapter", "chapter"): "adj",
+    ("chapter", "section"): "sub",
+    ("section", "section"): "adj",
+    ("section", "subsection"): "sub",
+    ("subsection", "subsection"): "adj",
+    ("subsection", "subsubsection"): "sub",
+    ("subsubsection", "subsubsection"): "adj",
 }
 
-sortable_envs = [
-    "Title",
-    "Text",
-    "Text-EQ",
-    "Equation",
-    "Footnote",
-    "List",
-    "PaperTitle",
-    "Abstract",
-]
+sortable_envs = ["Title", "Text", "Text-EQ", "Equation", "Footnote", "List"]
 
 
 threshold = 0.3
