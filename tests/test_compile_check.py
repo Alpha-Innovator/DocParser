83fa461487f806930a5c91d80ee10e436355d3cf
niladmiran <maosong@pjlab.org.cn>
2023-12-28 10:06:37 +0800

    test(tests/): add test for compile check

--- tests/test_compile_check.py
+++ tests/test_compile_check.py
@@ -11,3 +11,9 @@ class TestGraphics(unittest.TestCase):
 
         temp_files = [file for file in os.listdir(".") if file.startswith("temp")]
         self.assertEqual(len(temp_files), 0)
+
+    def test_equation2(self):
+        self.assertEqual(compile_check(r"\begin{equation}\e\end{equation}"), False)
+
+        temp_files = [file for file in os.listdir(".") if file.startswith("temp")]
+        self.assertEqual(len(temp_files), 0)
