ab73240ba9452ee0ca4c2c341c1acc2feeed080b
MaoSong2022 <maosong@pjlab.org.cn>
2024-01-09 11:34:14 +0800

    refactor(run_statistics.py): use pandas to extract result information

--- scripts/run_statistics.py
+++ scripts/run_statistics.py
@@ -1,9 +1,17 @@
 from collections import defaultdict
+import glob
 import os
+import re
 from typing import Dict
 import csv
+import pandas as pd
 
 from vrdu import utils
+from vrdu.config import config
+
+from vrdu import logger
+
+log = logger.setup_app_level_logger(file_name="statistics.log")
 
 
 def is_standalone(path: str) -> bool:
@@ -71,10 +79,8 @@ def analyze_result(path) -> Dict:
     return data
 
 
-if __name__ == "__main__":
-    data = analyze_result(
-        "/cpfs01/shared/ADLab/datasets/arxiv_source/arxiv_source_uncompressed/"
-    )
+def run_statistics_v1(path):
+    data = analyze_result(path)
     categories = utils.get_all_categories()
     with open("statistics.csv", "w") as f:
         fieldnames = ["category", "total", "successed"]
@@ -91,3 +97,134 @@ if __name__ == "__main__":
                 }
             )
     utils.export_to_json(data, "result_statistics.json")
+
+
+def run_statistics_v2():
+    all_categories = utils.get_all_categories()
+    columns = [
+        "uuid",
+        "title",
+        "category",
+        "path",
+        "status",
+        "error_type",
+        "error_info",
+        "date",
+        "pages",
+        "columns",
+        "blocks",
+        # *list(config.category2name.values()),
+        "overlap",
+    ]
+    df = pd.DataFrame(columns=columns)
+    log_files = glob.glob("batch_process_*.log")
+    for log_file in log_files:
+        category = "unknown"
+        with open(log_file, "r") as f:
+            for line in f.readlines():
+                if not line.startswith("["):
+                    continue
+                if line.find("Line 139") != -1:
+                    index = line.find("category: ")
+                    category = line[index + len("category: ") : -1]
+                    continue
+                if line.find("Line 74") != -1:
+                    index = line.find("processing file ")
+                    file = line[index + len("processing file ") :]
+                    path = os.path.dirname(file)
+                    category_path = os.path.basename(os.path.dirname(path))
+                    if category_path not in all_categories:
+                        continue
+
+                    pattern = r"(\d+\.\d+v\d+)\.(.*)"
+                    match = re.search(pattern, path)
+                    if not match:
+                        continue
+
+                    uuid = match.group(1)
+                    name = match.group(2)
+                    data = [
+                        uuid,
+                        name,
+                        category,
+                        path,
+                        "unknown",
+                        "",
+                        "",
+                        line[1:11],
+                        0,
+                        0,
+                        0,
+                        0.0,
+                    ]
+                    df.loc[len(df)] = data
+                    log.debug(f"data numbers: {len(df)}")
+                    continue
+                if line.find("Line 79") != -1:
+                    file = line.split("File ")[1].split(" ")[0]
+                    path = os.path.dirname(file)
+                    category_path = os.path.basename(os.path.dirname(path))
+                    if category_path not in all_categories:
+                        continue
+
+                    pattern = r"(\d+\.\d+v\d+)\.(.*)"
+                    match = re.search(pattern, path)
+                    if not match:
+                        continue
+
+                    uuid = match.group(1)
+                    name = match.group(2)
+                    existed_index = df[df["uuid"] == uuid].index
+
+                    df.loc[existed_index, "status"] = "success"
+                    continue
+                if line.find("Line 103") != -1:
+                    index = line.find("processing file ")
+                    file = line[index + len("processing file ") :]
+                    path = os.path.dirname(file)
+                    category_path = os.path.basename(os.path.dirname(path))
+                    if category_path not in all_categories:
+                        continue
+
+                    pattern = r"(\d+\.\d+v\d+)\.(.*)"
+                    match = re.search(pattern, path)
+                    if not match:
+                        continue
+
+                    uuid = match.group(1)
+                    name = match.group(2)
+                    existed_index = df[df["uuid"] == uuid].index
+
+                    df.loc[existed_index, "status"] = "success"
+                    continue
+                if line.find("Line 108") != -1:
+                    # Extract variables using split()
+                    file_name = line.split("processing file ")[1].split(",")[0]
+                    error_type = line.split("type: ")[1].split(",")[0]
+                    error_info = line.split("message: ")[1][:-1]
+                    path = os.path.dirname(file_name)
+                    category_path = os.path.basename(os.path.dirname(path))
+                    if category_path not in all_categories:
+                        continue
+
+                    pattern = r"(\d+\.\d+v\d+)\.(.*)"
+                    match = re.search(pattern, path)
+                    if not match:
+                        continue
+
+                    uuid = match.group(1)
+                    name = match.group(2)
+                    existed_index = df[df["uuid"] == uuid].index
+
+                    df.loc[existed_index, "status"] = "failure"
+                    df.loc[existed_index, "error_type"] = error_type
+                    df.loc[existed_index, "error_info"] = error_info
+                    continue
+
+    df.to_csv("data.csv", index=False)
+
+
+if __name__ == "__main__":
+    # path = "/cpfs01/shared/ADLab/datasets/arxiv_source/arxiv_source_uncompressed/"
+    # run_statistics_v1(path)
+    run_statistics_v2()
