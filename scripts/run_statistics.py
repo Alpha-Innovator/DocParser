ae6d44895b3b233dbbefa9312fb9c40babd51501
MaoSong2022 <maosong@pjlab.org.cn>
2024-03-19 15:10:19 +0800

    feat(run_statistics.py): simplify code logic
    1. remove unnecessary fields
    2. fix repeat item bugs
    3. fix uuid type bugs

--- scripts/run_statistics.py
+++ scripts/run_statistics.py
@@ -1,9 +1,9 @@
 import glob
 import os
-import re
 import pandas as pd
 from datetime import datetime
 import argparse
+from datetime import datetime
 
 from vrdu import utils
 from vrdu.config import config
@@ -13,7 +13,9 @@ from vrdu import logger
 log = logger.setup_app_level_logger(file_name="statistics.log")
 
 
-data_file = "data/processed_paper_database.csv"
+database_file = "data/processed_paper_database.csv"
+daily_overview_file = "data/daily_overview.csv"
+discpline_info_file = "data/discpline_info.csv"
 
 
 def extract_time(line: str) -> datetime:
@@ -23,8 +25,8 @@ def extract_time(line: str) -> datetime:
 
 
 def init_dataframe() -> pd.DataFrame:
-    if os.path.exists(data_file):
-        return pd.read_csv(data_file)
+    if os.path.exists(database_file):
+        return pd.read_csv(database_file, dtype={"uuid": str})
     columns = [
         "uuid",
         "discpline",
@@ -44,7 +46,7 @@ def init_dataframe() -> pd.DataFrame:
     return df
 
 
-def run_statistics(input_path: str):
+def update_processed_database(input_path: str):
     """store the information of processed papers into a csv file"""
     df = init_dataframe()
 
@@ -75,15 +77,17 @@ def run_statistics(input_path: str):
 
             # extract uuid and title
             uuid = os.path.basename(path)
-
-            index = df[df["uuid"] == uuid].index
+            log.debug(f"uuid: {uuid}")
             current_time = extract_time(line)
 
             # new file
             if line.find("start processing") != -1:
-                if not index.empty:
+                if uuid in df["uuid"].values:
                     continue
+
+                log.debug(f"new file: {tex_file} with uuid: {uuid}")
                 data_item = {
+                    "index": len(df),
                     "uuid": uuid,
                     "discpline": discpline,
                     "path": path,
@@ -102,18 +106,29 @@ def run_statistics(input_path: str):
                 continue
 
             # success processing file, update information
-            if line.find("successfully processed") != -1:
-                df.loc[index, "status"] = "success"
-                df.loc[index, "end_time"] = current_time
+            if (
+                line.find("successfully processed") != -1
+                or line.find("paper has been processed") != -1
+            ):
+                if uuid in df["uuid"].values:
+                    if df[df["uuid"] == uuid]["status"] == "success":
+                        continue
+                df.loc[df["uuid"] == uuid, "status"] = "success"
+                df.loc[df["uuid"] == uuid, "end_time"] = current_time
                 continue
+
             # failed to process file, update status and eror information
             if line.find("message: ") != -1:
-                df.loc[index, "status"] = "failure"
+                if uuid in df["uuid"].values:
+                    if df[df["uuid"] == uuid]["status"] == "failure":
+                        continue
                 error_type = line.split("type: ")[1].split(", ")[0]
                 error_info = line.split("message: ")[1].strip()
-                df.loc[index, "error_type"] = error_type
-                df.loc[index, "error_info"] = error_info
-                df.loc[index, "end_time"] = current_time
+
+                df.loc[df["uuid"] == uuid, "status"] = "failure"
+                df.loc[df["uuid"] == uuid, "error_type"] = error_type
+                df.loc[df["uuid"] == uuid, "error_info"] = error_info
+                df.loc[df["uuid"] == uuid, "end_time"] = current_time
                 continue
 
     category_names = list(config.category2name.values())
@@ -126,11 +141,12 @@ def run_statistics(input_path: str):
 
         if df.loc[index, "pages"] != 0:
             continue
+
         # use output result to update information
         path = df.loc[index, "path"]
-        result_path = os.path.join(path, "output/result")
-        quality_report_file = os.path.join(result_path, "quality_report.json")
-        quality_report = utils.load_json(quality_report_file)
+        quality_report = utils.load_json(
+            os.path.join(path, "output/result/quality_report.json")
+        )
         df.loc[index, "pages"] = quality_report["num_pages"]
         df.loc[index, "columns"] = quality_report["num_columns"]
         df.loc[index, "blocks"] = quality_report["category_quality"][-1][
@@ -138,22 +154,98 @@ def run_statistics(input_path: str):
         ]
         df.loc[index, "overlap"] = quality_report["page_quality"][-1]["ratio"]
 
-        for item in quality_report["category_quality"]:
-            if item["category"] in category_names:
-                df.loc[index, item["category"]] = item["geometry_count"]
+    # remove processing files
+    df = df[~(df["status"] == "processing")]
+    df.to_csv(database_file, index=False)
 
-        layout_annotation_file = os.path.join(result_path, "layout_annotation.json")
-        layout_annotation = utils.load_json(layout_annotation_file)
-        df.loc[index, "date"] = layout_annotation["info"]["date_created"]
 
-    df.to_csv(data_file)
+def update_discpline_info():
+    df = pd.read_csv(discpline_info_file)
+
+    for log_file in glob.glob("data/batch_process_*.log"):
+        discpline = (
+            os.path.basename(log_file).split("batch_process_")[1].split(".log")[0]
+        )
+        with open(log_file) as f:
+            lines = f.readlines()
+        for line in lines:
+            if line.find("[VRDU] Before filtering") != -1:
+                processable_files = int(line.split("found ")[1].split(" ")[0])
+                log.debug(
+                    f"discpline: {discpline}, processable files: {processable_files}"
+                )
+                df.loc[df["discpline"] == discpline, "num_papers"] = processable_files
+
+            if line.find("finished processing.") != -1:
+                df.loc[df["discpline"] == discpline, "status"] = "complete"
+            else:
+                df.loc[df["discpline"] == discpline, "status"] = "processing"
+
+    database_df = pd.read_csv(database_file)
+    for index, row in df.iterrows():
+        df.loc[index, "success"] = len(
+            database_df[
+                (database_df["discpline"] == row["discpline"])
+                & (database_df["status"] == "success")
+            ]
+        )
+        df.loc[index, "failure"] = len(
+            database_df[
+                (database_df["discpline"] == row["discpline"])
+                & (database_df["status"] == "failure")
+            ]
+        )
+        processed_papers = len(
+            database_df[(database_df["discpline"] == row["discpline"])]
+        )
+
+        df.loc[index, "processed"] = processed_papers
+
+    df.to_csv(discpline_info_file, index=False)
+
+
+def update_daily_overview() -> None:
+    daily_df = pd.read_csv(daily_overview_file)
+    database_df = pd.read_csv(database_file)
+
+    num_total_papers = database_df.shape[0]
+    num_total_processed = database_df[database_df["status"] == "success"].shape[0]
+
+    last_index = daily_df.index[-1]
+    num_daily_papers = num_total_papers - daily_df.loc[last_index, "#total papers"]
+    num_daily_processed = (
+        num_total_processed - daily_df.loc[last_index, "#total processed"]
+    )
+
+    if num_total_papers == daily_df.loc[last_index, "#total papers"]:
+        log.info("Please update database file before running this script.")
+
+    daily_df.loc[last_index + 1, "date"] = datetime.today().strftime("%Y-%m-%d")
+    daily_df.loc[last_index + 1, "#daily papers"] = num_daily_papers
+    daily_df.loc[last_index + 1, "#daily processed"] = num_daily_processed
+    daily_df.loc[last_index + 1, "#total papers"] = num_total_papers
+    daily_df.loc[last_index + 1, "#total processed"] = num_total_processed
+    daily_df.loc[last_index + 1, "#discplines"] = database_df["discpline"].nunique()
+    daily_df["daily pass ratio"] = (
+        daily_df["#daily processed"] / daily_df["#daily papers"]
+    )
+    daily_df["total pass ratio"] = (
+        daily_df["#total processed"] / daily_df["#total papers"]
+    )
+
+    daily_df.to_csv("data/daily_overview.csv", index=False)
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--input_path", type=str, default="data/")
     args = parser.parse_args()
-    run_statistics(args.input_path)
+
+    update_processed_database(args.input_path)
+
+    update_daily_overview()
+
+    update_discpline_info()
 
 
 if __name__ == "__main__":
