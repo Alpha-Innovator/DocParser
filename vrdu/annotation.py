fa445bd16670ebfb1eb368107dab03daec5052dc
MaoSong2022 <maosong@pjlab.org.cn>
2024-03-27 17:43:10 +0800

    fix(annotation.py): variable shadow error

--- vrdu/annotation.py
+++ vrdu/annotation.py
@@ -650,13 +650,13 @@ class OrderAnnotation:
             if config.category2name[block["category"]] not in ["Text", "Text-EQ"]:
                 continue
             block["references"] = re.findall(ref_patterns, block["source_code"])
-            for label in block["references"]:
-                if label in label_to_block_id:
+            for _label in block["references"]:
+                if _label in label_to_block_id:
                     annotations.append(
                         {
                             "type": "explicit-cite",
                             "from": block["block_id"],
-                            "to": label_to_block_id[label],
+                            "to": label_to_block_id[_label],
                         }
                     )
 
