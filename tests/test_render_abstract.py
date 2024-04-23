cd8bc741b8d69e4d46f1ffcd6ac38a0b9031dc02
MaoSong2022 <maosong@pjlab.org.cn>
2024-04-23 14:55:35 +0800

    fix(test_render_abstract.py): abstract render logic changes error

--- tests/test_render_abstract.py
+++ tests/test_render_abstract.py
@@ -33,7 +33,7 @@ class TestAbstract(unittest.TestCase):
             self.renderer.render_abstract(file_mock)
             file_mock.assert_called_with(file_mock, "w")
             file_mock().write.assert_called_with(
-                """\\documentclass{article}\\begin{document}\\begin{abstract}{\\color{Abstract_color}\n   This is the content of abstract.\n }\\end{abstract}\\end{document}"""
+                """\\documentclass{article}\\begin{document}{\\begin{abstract}\\color{Abstract_color}\n   This is the content of abstract.\n \\end{abstract}}\\end{document}"""
             )
 
     def test_one_abstract2(self):
@@ -45,5 +45,5 @@ class TestAbstract(unittest.TestCase):
             self.renderer.render_abstract(file_mock)
             file_mock.assert_called_with(file_mock, "w")
             file_mock().write.assert_called_with(
-                "\\begin{abstract}{\\color{Abstract_color}\nWe consider the projection onto the $\\ell_{1, \\infty}$ norm ball for solving group sparse optimization problems arising in multi-task learning. \nBased on the primal-dual gradient method (PDG), we present a novel primal-dual Newton method, which updates the primal and dual iterates incrementally with Newton steps. \nWe exploit the problem-inherent structure so that the closed-form for the primal-dual Newton steps can be derived easily. \nCompared with existing algorithms, our approach does not need to maintain the primal feasibility, nor require the computation of the $\\ell_1$ ball projection subproblems. \nWe prove that our algorithm terminates after a finite number of iterations. \nNumerical simulations on synthetic and real-world data show that our proposed method is faster than the previous state-of-the-art.\n\n}\\end{abstract}"
+                "{\\begin{abstract}\\color{Abstract_color}\nWe consider the projection onto the $\\ell_{1, \\infty}$ norm ball for solving group sparse optimization problems arising in multi-task learning. \nBased on the primal-dual gradient method (PDG), we present a novel primal-dual Newton method, which updates the primal and dual iterates incrementally with Newton steps. \nWe exploit the problem-inherent structure so that the closed-form for the primal-dual Newton steps can be derived easily. \nCompared with existing algorithms, our approach does not need to maintain the primal feasibility, nor require the computation of the $\\ell_1$ ball projection subproblems. \nWe prove that our algorithm terminates after a finite number of iterations. \nNumerical simulations on synthetic and real-world data show that our proposed method is faster than the previous state-of-the-art.\n\n\\end{abstract}}"
             )
