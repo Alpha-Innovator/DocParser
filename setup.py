3ab4efbb7952cf40aea009033cbd79ae71e15184
MaoSong2022 <maosong@pjlab.org.cn>
2024-06-13 10:28:23 +0800

    docs(setup.py): update metadata

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name="vrdu_data_process",
-    version="0.5.0",
+    version="1.0.0",
     description="process the academic papers with .tex source files",
     author="Mao Song",
     author_email="maosong@pjlab.org.cn",
-    url="https://github.com/MaoSong2022/vrdu_data_process",
+    url="https://github.com/UniModal4Reasoning/DocParser.git",
     license="MIT",
     packages=find_packages(),
     install_requires=[
@@ -16,7 +16,6 @@ setup(
         "numpy==1.24.3",
         "pdf2image==1.16.3",
         "pdfminer.six==20221105",
-        # "Pillow==9.4.0",
         "Pillow==10.1.0",
         "pyparsing==3.1.1",
         "pytest==7.4.2",
@@ -24,9 +23,7 @@ setup(
         "setuptools==68.0.0",
         "tqdm==4.66.1",
     ],
-    scripts=[
-        "vrdu/compile_latex.sh",
-    ],
+    scripts=[],
     entry_points={
         "console_scripts": [],
     },
