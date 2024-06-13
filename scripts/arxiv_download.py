3d5f83299e107fd0715969056b09458d59423263
MaoSong2022 <maosong@pjlab.org.cn>
2024-06-13 16:26:15 +0800

    refactor(arxiv_download.py): simplify code logic

--- scripts/arxiv_download.py
+++ scripts/arxiv_download.py
@@ -1,84 +1,120 @@
+import argparse
 import arxiv
 import os
-from typing import List, Dict
-from tqdm import tqdm
+from typing import List
 import tarfile
 
 
-from DocParser.vrdu import utils
 from DocParser.vrdu import logger
 
-log = logger.setup_app_level_logger(file_name="arxiv_download.log")
+
+log = logger.setup_app_level_logger(logger_name="arxiv_download.log")
 
 
-def arxiv_download(data: List[Dict], path: str) -> None:
-    """Download papers from arXiv based on category data.
+def download_papers_with_paper_id(
+    path: str, discipline: str, paper_ids: List[str]
+) -> None:
+    """
+    Downloads papers from the Arxiv repository based on the specified paper IDs.
 
-    This function takes a list of category download tasks and a base
-    path. For each category item in the data, it will:
-
-    1. Create a subdirectory under the given path for that category
-    2. Check if there are already enough papers in the subdir
-    3. Search arXiv for the category
-    4. Download up to the requested count of newest papers
-    5. Save each paper to the category subdirectory
-
-    Arguments:
-        data (List[Dict]): List of dicts with keys "category" and "count"
-        path (str): Base directory path to save papers
+    Args:
+    - path (str): The path where the downloaded papers will be saved.
+    - discipline (str): The discipline of the papers to be downloaded.
+    - paper_ids (List[str]): A list of paper IDs to be downloaded.
 
     Returns:
-        None
+    None
+
+    Raises:
+    - tarfile.ReadError: cannot unpack the .tar.gz file
+
+    Usage:
+    ```python
+    download_papers_with_paper_id(path, discipline, paper_ids)
+    ```
+
     """
     client = arxiv.Client()
-    for row in tqdm(data):
-        if row["auto_annotated_paper_path"]:
-            continue
-        discipline = row["discipline"]
-        discipline_path = os.path.join(path, discipline)
-        os.makedirs(discipline_path, exist_ok=True)
+    discipline_path = os.path.join(path, discipline)
+    os.makedirs(discipline_path, exist_ok=True)
 
-        if os.path.exists(os.path.join(discipline_path, row["paper_id"])):
-            log.debug(f'{os.path.join(discipline_path, row["paper_id"])} exists')
+    search_results = client.results(arxiv.Search(id_list=paper_ids))
+
+    for result in search_results:
+        # extract {id} without version from http://arxiv.org/abs/{id}
+        paper_id = result.entry_id.split("/")[1].split("v")[0]
+        log.info(f"Downloading paper {paper_id}")
+
+        tar_file_path = result.download_source(dirpath=discipline_path)
+        log.info(f"Downloading tar file {tar_file_path}")
+        paper_path = os.path.join(discipline_path, paper_id)
+        if os.path.exists(paper_path):
             continue
 
-        if os.path.exists(os.path.join(discipline_path, row["paper_id"], ".tar.gz")):
-            log.debug(
-                f'{os.path.join(discipline_path, row["paper_id"], ".tar.gz")} exists'
-            )
+        try:
+            with tarfile.open(tar_file_path, "r:gz") as tar:
+                tar.extractall(paper_path)
+        except tarfile.ReadError:
+            log.error(f"{tar_file_path} is not a tar.gz file")
             continue
 
-        search_results = client.results(arxiv.Search(id_list=[row["paper_id"]]))
+
+def download_batch_papers(path: str, discipline: str, num_papers: int) -> None:
+    """
+    Downloads a batch of papers from the Arxiv repository
+    based on the specified discipline and number of papers.
+
+    Args:
+    - path (str): The path where the downloaded papers will be saved.
+    - discipline (str): The discipline of the papers to be downloaded.
+    - num_papers (int): The number of papers to be downloaded.
+
+    Returns:
+    None
+
+    Raises:
+    None
+
+    Usage:
+    ```python
+    download_batch_papers(output_path, discipline, num_papers)
+    ```
+
+    """
+    client = arxiv.Client()
+
+    paper_ids = []
+    while num_papers > 0:
+        search_results = client.results(
+            arxiv.Search(query=discipline, max_results=num_papers)
+        )
 
         for result in search_results:
-            tar_file_path = result.download_source(dirpath=discipline_path)
-            log.debug(f"Downloading tar file {tar_file_path}")
-            paper_path = os.path.join(discipline_path, row["paper_id"])
-            try:
-                with tarfile.open(tar_file_path, "r:gz") as tar:
-                    tar.extractall(paper_path)
-            except tarfile.ReadError:
-                log.error(f"{tar_file_path} is not a tar.gz file")
-                continue
+            paper_id = result.entry_id.split("/")[1].split("v")[0]
+            log.debug(f"Downloading paper {paper_id}")
+            if paper_id not in paper_ids:
+                paper_ids.append(paper_id)
+                num_papers -= 1
+
+    download_papers_with_paper_id(path, discipline, paper_ids)
 
 
-def main():
-    import argparse
-
+def main() -> None:
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "-p", "--path", type=str, required=True, help="Path to save result"
     )
     parser.add_argument(
-        "-f", "--file", type=str, required=True, help="json file for saving result"
+        "-d", "--discipline", type=str, default="cs.CV", help="discipline to download"
+    )
+    parser.add_argument(
+        "-i", "--num_papers", type=int, default=1, help="Number of paper to download"
     )
 
     args = parser.parse_args()
-    output_path, json_file = args.path, args.file
+    output_path, discipline, num_papers = args.path, args.discipline, args.num_papers
 
-    json_data = utils.load_json(json_file)
-
-    arxiv_download(json_data, output_path)
+    download_batch_papers(output_path, discipline, num_papers)
 
 
 if __name__ == "__main__":
