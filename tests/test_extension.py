c565dfccf5aeb8e66a3811e34ba0343cc000c797
CHEN YANG <1402375027@qq.com>
2024-06-21 18:30:53 +0800

    test(test_extension): modify the content to test the function of replace_figures_in_tex_files

--- tests/test_extension.py
+++ tests/test_extension.py
@@ -1,60 +1,51 @@
 import unittest
-import os
 import unittest.mock
-
-
-from replace_figure_extension import replace_figures_extension_with_png
-
-
+from DocParser.vrdu.preprocess import replace_figures_in_tex_files
 class TestAbstract(unittest.TestCase):
     def setUp(self) -> None:
-        
-        # 测试环境的设置，包括创建测试文件夹和文件
-        self.test_dir = 'test_directory'
-        self.original_tex = os.path.join(self.test_dir, 'test.tex')
-        os.makedirs(self.test_dir, exist_ok=True)
-        with open(self.original_tex, 'w') as f:
-            f.write(r'''
-                    \\begin{figure}[ht]
-                    \\centerline{\\includegraphics[width=\\columnwidth]{figures/time_vs_dimension.pdf}}
-                    \\subfigure[]{\\includegraphics[width=0.48\\columnwidth]{figures/iterate_constraint.jpg}} 
-                    \\subfigure[]{\\epsfig{figures/iterate_error.eps}} 
-                    \\subfigure[]{\\psfig[width=0.48\\columnwidth]{figures/time_constraint.ps}}
-                    \\subfigure[]{\\includegraphics[width=0.48\\columnwidth]{figures/iterate_correct.png}}
-                    \\subfigure[]{\\includegraphics[width=0.5\\columnwidth]{figures/time_error}}
-                    \\subfigure[]{\\includegraphics[width=0.5\\columnwidth]{figures/time_error_1}}
-                    \\label{fig:iteration_information}
-                    ''')
+        self.initial_content = """
+            \\begin{figure}[ht]
+            \\centerline{\\includegraphics[width=\\columnwidth]{dir1/time_vs_dimension.pdf}}
+            \\subfigure[]{\\includegraphics[width=0.48\\columnwidth]{dir2/iterate_constraint.jpg}} 
+            \\subfigure[]{\\epsfig{dir2/iterate_error.eps}} 
+            \\subfigure[]{\\psfig[width=0.48\\columnwidth]{time_constraint.es}}
+            \\subfigure[]{\\includegraphics[width=0.48\\columnwidth]{dir3/dir4/iterate_correct.png}}
+            \\subfigure[]{\\includegraphics[width=0.5\\columnwidth]{dir3/time_error}}
+            \\subfigure[]{\\includegraphics[width=0.5\\columnwidth]{dir3/time_error_1}}
+            \\label{fig:iteration_information}
+        """
 
-        # 模拟图片文件
-        self.image_files = [
-            'time_vs_dimension.pdf', 'iterate_constraint.jpg', 'iterate_error.eps', 'time_constraint.ps', 'iterate_correct.png', 'time_error.pdf', 'time_error_1.jpeg'
-        ]
-        for file_name in self.image_files:
-            with open(os.path.join(self.test_dir, file_name), 'w') as f:
-                f.write('dummy content')
+        # Simulate image files with correct extensions
+        self.image_files = {
+            'time_vs_dimension': 'dir1/time_vs_dimension.pdf',
+            'iterate_constraint': 'dir2/iterate_constraint.jpg',
+            'iterate_error': 'dir2/iterate_error.eps',
+            'time_constraint': 'time_constraint.es',
+            'iterate_correct': 'dir3/dir4/iterate_correct.png',
+            'time_error': 'dir3/time_error.pdf',
+            'time_error_1': 'dir3/time_error_1.jpeg'
+        }
 
-    def tearDown(self):
-        # 清理测试创建的文件和目录
-        for root, dirs, files in os.walk(self.test_dir, topdown=False):
-            for name in files:
-                os.remove(os.path.join(root, name))
-            for name in dirs:
-                os.rmdir(os.path.join(root, name))
-        os.rmdir(self.test_dir)
+    def test_replace_figures(self):
+        expected_content = """
+            \\begin{figure}[ht]
+            \\centerline{\\includegraphics[width=\\columnwidth]{dir1/time_vs_dimension.png}}
+            \\subfigure[]{\\includegraphics[width=0.48\\columnwidth]{dir2/iterate_constraint.jpg}} 
+            \\subfigure[]{\\includegraphics{dir2/iterate_error.png}} 
+            \\subfigure[]{\\includegraphics[width=0.48\\columnwidth]{time_constraint.png}}
+            \\subfigure[]{\\includegraphics[width=0.48\\columnwidth]{dir3/dir4/iterate_correct.png}}
+            \\subfigure[]{\\includegraphics[width=0.5\\columnwidth]{dir3/time_error.png}}
+            \\subfigure[]{\\includegraphics[width=0.5\\columnwidth]{dir3/time_error_1.jpeg}}
+            \\label{fig:iteration_information}
+        """
 
-    def test(self):
-        replace_figures_extension_with_png(self.original_tex)
-        with open(self.original_tex, 'r') as f:
-            content = f.read()
-        self.assertEqual(content, r'''
-                    \\begin{figure}[ht]
-                    \\centerline{\\includegraphics[width=\\columnwidth]{figures/time_vs_dimension.png}}
-                    \\subfigure[]{\\includegraphics[width=0.48\\columnwidth]{figures/iterate_constraint.jpg}} 
-                    \\subfigure[]{\\includegraphics{figures/iterate_error.png}} 
-                    \\subfigure[]{\\includegraphics[width=0.48\\columnwidth]{figures/time_constraint.png}}
-                    \\subfigure[]{\\includegraphics[width=0.48\\columnwidth]{figures/iterate_correct.png}}
-                    \\subfigure[]{\\includegraphics[width=0.5\\columnwidth]{figures/time_error.png}}
-                    \\subfigure[]{\\includegraphics[width=0.5\\columnwidth]{figures/time_error_1.jpeg}}
-                    \\label{fig:iteration_information}
-                    ''')
+        with unittest.mock.patch(
+            "builtins.open",
+            new=unittest.mock.mock_open(read_data=self.initial_content),
+            create=True,
+        ) as file_mock:
+            replace_figures_in_tex_files(file_mock,self.image_files)
+            file_mock.assert_called_with(file_mock, "w")
+            file_mock().write.assert_called_with(
+                expected_content
+            )
